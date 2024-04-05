c3fea67b04301b5f04bab532382d732eb41392af
Anton Merkelsen <55212514+Antwarn@users.noreply.github.com>
2024-04-05 14:03:15 +0200

    Opdateret med udregninger
    Skal have lavet et if statement der siger om den skal udregnes posetivt eller negativt.

--- stocklist.py
+++ stocklist.py
@@ -12,9 +12,22 @@ class StockInfoAnalyzer:
                 prev_index = self.line_number - 6
                 prev_price = self.df.iloc[prev_index, 2]  # Assuming the price is in the third column
                 current_price = self.df.iloc[self.line_number, 2]  # Assuming the price is in the third column
-                change = ((current_price - prev_price) / prev_price) * 100
+                
+                inc = prev_price - current_price
+                pos_change = ((prev_price - current_price)/current_price)*100 
+                Dec = current_price - prev_price
+                neg_change = abs(((Dec)/prev_price)*100)
+                
+                print(neg_change)
+                print(Dec)
+                print(f"Increase: {inc:.2f}$")
+                print(f"Positive change: {pos_change:.2f}")
+                print(f"Previous price: {prev_price:.2f}")
+                print(f"Current price: {current_price:.2f}")    
+                print(f'Pos change: {pos_change}%')
+                #change = ((current_price - prev_price) / prev_price) * 100
                 comp = self.df.iloc[prev_index, 1]  # Assuming the company name is in the second column
-                print(f'Comp: {comp}, Price: {current_price}, {prev_price} Change: {change}')
+                #print(f'Comp: {comp}, Price: {current_price}, {prev_price} Change: {change}')
                 #print(f'Prev: {prev_price} Line: {self.line_number} Prev_index: {prev_index} Current: {current_price}') 
                 #print(f"Change for company {comp} in line {self.line_number}: {change}")
 
@@ -24,5 +37,4 @@ if __name__ == "__main__":
     analyzer.analyze()
 
 
-'''Problemet er at de tager den forkerte linje i programmet og udregner change, Tror problemet ligger på linje 
-6. Der udover tror jeg at den tager prisen fra den samme linje heletiden og derfor er der ikke nogen change.'''
+'''Skal have lavet et if statement der siger om den skal udregnes posetivt eller negativt.'''
