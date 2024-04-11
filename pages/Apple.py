695ee79b0ac2f381b876debd4d85691641668fff
Mumlemads <mads.lauge8@gmail.com>
2024-04-11 12:04:06 +0200

    CSV reading

--- pages/Apple.py
+++ pages/Apple.py
@@ -1,9 +1,43 @@
 import yfinance as yf
 import streamlit as st
+import matplotlib
+import csv
+import numpy as np
+
+
 
 def display_apple():
     st.header("Apple")
-    st.write("### Apple's aktier fra starten af 2023")
+    
+    with open('stockinfo.csv', 'r') as csvfile:
+
+
+        data = []
+    
+        csv_reader = csv.reader(csvfile)
+
+        line_number = 0
+
+        for row in csv_reader:
+            line_number += 1
+            if line_number % 6 == 0:
+                
+
+        
+                price = row[2]
+                volume = row[3]
+                mc = row[4]
+                x = [price, volume, mc]
+                data.append(x)
+
+
+
+
+
+
+
+
+    '''st.write("### Apple's aktier fra starten af 2023")
     apple = 'aapl'
     #get data on this ticker
     tickerData = yf.Ticker(apple)
@@ -12,4 +46,4 @@ def display_apple():
     # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
     st.line_chart(tickerDf.Close)
-    #st.line_chart(tickerDf.Volume)
+    #st.line_chart(tickerDf.Volume)'''
--- pages/data.py
+++ pages/data.py
@@ -4,13 +4,20 @@ import pandas
 
 
 
-with open('stockinfo_navne.csv', 'r') as csvfile:
+with open('stockinfo.csv', 'r') as csvfile:
     csv_reader = csv.reader(csvfile)
+    data = []
+
+    line_number = 0
 
     for row in csv_reader:
-        price = row[2]
-        volume = row[3]
-        mc = row[4]
-        print(price, volume, mc)
+        line_number += 1
+        if line_number % 6 == 0:
+        
+            price = row[2]
+            volume = row[3]
+            mc = row[4]
+            x = [price, volume, mc]
+            data.append(x)
 
-
+    print(data)
