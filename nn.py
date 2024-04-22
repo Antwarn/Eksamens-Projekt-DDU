b9dd425fefd1377dd2b221cdd390ec3ab368c74b
Anton Merkelsen <55212514+Antwarn@users.noreply.github.com>
2024-04-22 12:55:42 +0200

    Jajaja

--- nn.py
+++ nn.py
@@ -1,69 +1,67 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import LSTM, Dropout, Dense
 
-data = 'stockinfo.csv'
-dataset_train = pd.read_csv(data)
+# Load the data
+data = './output/Alphabet Inc Class A (GOOGL).csv'
+dataset = pd.read_csv(data)
+# Remove 'Company' column from the dataset
+dataset_train = dataset.drop(columns=['company'])
 training_set = dataset_train.iloc[:, 1:2].values
 
-dataset_train.head()
-
-from sklearn.preprocessing import MinMaxScaler
+# Feature scaling
 sc = MinMaxScaler(feature_range=(0,1))
 training_set_scaled = sc.fit_transform(training_set)
 
+# Prepare training data
 X_train = []
 y_train = []
-
-for i in range(60, 2035):
+for i in range(60, len(training_set)):
     X_train.append(training_set_scaled[i-60:i, 0])
     y_train.append(training_set_scaled[i, 0])
-
 X_train, y_train = np.array(X_train), np.array(y_train)
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 
-from keras.models import Sequential
-from keras.layers import LSTM
-from keras.layers import Dropout
-from keras.layers import Dense
-
+# Build the LSTM model
 model = Sequential()
-model.add(LSTM(units=50,return_sequences=True,input_shape=(X_train.shape[1], 1)))
+model.add(LSTM(units=50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
 model.add(Dropout(0.2))
-model.add(LSTM(units=50,return_sequences=True))
+model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
-model.add(LSTM(units=50,return_sequences=True))
+model.add(LSTM(units=50, return_sequences=True))
 model.add(Dropout(0.2))
 model.add(LSTM(units=50))
 model.add(Dropout(0.2))
 model.add(Dense(units=1))
-model.compile(optimizer='adam',loss='mean_squared_error')
-model.fit(X_train,y_train,epochs=100,batch_size=32)
 
-data = pd.read_csv('stockinfo.csv')
-dataset_test = pd.read_csv(data)
-real_stock_price = dataset_test.iloc[:, 1:2].values
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
 
-dataset_total = pd.concat((dataset_train['Open'], dataset_test['Open']), axis = 0)
-inputs = dataset_total[len(dataset_total) - len(dataset_test) - 60:].values
-inputs = inputs.reshape(-1,1)
+# Train the model
+model.fit(X_train, y_train, epochs=100, batch_size=32)
+
+# Prepare test data
+dataset_total = pd.concat((dataset_train['Open'], dataset['Open']), axis=0)
+inputs = dataset_total[len(dataset_total) - len(dataset) - 60:].values.reshape(-1,1)
 inputs = sc.transform(inputs)
 X_test = []
-
-for i in range(60, 76):
+for i in range(60, len(inputs)):
     X_test.append(inputs[i-60:i, 0])
-
-X_test = np.array(X_test)  # Convert the list to a NumPy array
+X_test = np.array(X_test)
 X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
 
-    
+# Predictions
 predicted_stock_price = model.predict(X_test)
 predicted_stock_price = sc.inverse_transform(predicted_stock_price)
 
-plt.plot(real_stock_price, color = 'black', label = 'TATA Stock Price')
-plt.plot(predicted_stock_price, color = 'green', label = 'Predicted TATA Stock Price')
-plt.title('TATA Stock Price Prediction')
+# Visualize the results
+plt.plot(dataset['Open'].values, color='black', label='Real Stock Price')
+plt.plot(predicted_stock_price, color='green', label='Predicted Stock Price')
+plt.title('Stock Price Prediction')
 plt.xlabel('Time')
-plt.ylabel('TATA Stock Price')
+plt.ylabel('Stock Price')
 plt.legend()
 plt.show()
--- splitcsv.py
+++ splitcsv.py
@@ -1,22 +1,20 @@
 import pandas as pd
+import os
 
 input_file = 'stockinfo.csv'
 output_folder = 'output'
 
 # Create output folder if it doesn't exist
-import os
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
 # Read the input CSV file
 df = pd.read_csv(input_file)
 
-# Set the number of companies here
-num_companies = 6
+# Get unique company names
+unique_companies = df['company'].unique()
 
-# Split the data into separate dataframes based on company ID
-company_dfs = [df[df['company'] == i] for i in range(num_companies)]
-
-# Write the output CSV files
-for i, company_df in enumerate(company_dfs):
-    company_df.to_csv(f'{output_folder}/company_{i}.csv', index=False)
+# Write separate CSV files for each company
+for company_name in unique_companies:
+    company_df = df[df['company'] == company_name]
+    company_df.to_csv(f'{output_folder}/{company_name}.csv', index=False)
